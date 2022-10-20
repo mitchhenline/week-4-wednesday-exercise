8f6484d3ca9517634d8228599a3be858b71200da
Mitch Henline <rmhenline@gmail.com>
2022-10-19 23:28:14 -0600

    done and functioning

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 const button = document.querySelector('#get-residents')
-const residentNames = document.querySelector('#resident-names')
-const residentNames2 = document.querySelector('#resident-names2')
-const residentNames3 = document.querySelector('#resident-names3')
+const nameSpace = document.querySelector('#name-return')
 
 function clickButton(){
     console.log('button clicked')
     axios.get('https://swapi.dev/api/planets/2/')
     .then((response) => {
         let {residents} = response.data
-        for(i = 0; i < residents.length; i++){
-        let person1 = axios.get('https://swapi.dev/api/people/5/')
-        let person2 = axios.get('https://swapi.dev/api/people/68/')
-        let person3 = axios.get('https://swapi.dev/api/people/81/')
+        for(let i = 0; i < residents.length; i++) {
+            let ul = document.createElement('ul')
+            let li = document.createElement('li')
+
+            axios.get(residents[i]).then((res) => {
+                let {name} = res.data
+                li.textContent = name
+            }).catch((error) => {
+                console.log('error')
+            })
+        
+        ul.appendChild(li)
+        nameSpace.appendChild(ul)
+
+    
+    
         }
 
-        residentNames.textContent = (person1)
-
-    .then()
-
     })
-    .catch((error) => {
-        console.log('error')
-    })
-    
+
 }
 
 button.addEventListener("click", clickButton)
